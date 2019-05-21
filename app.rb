ce726c54ae6a387b927cb32ad5596d658892db7b
Ming Duan <11203duan@gmail.com>
2019-05-21 17:33:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,11 +3,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-
   end
 
   get '/reversename/:name' do
     erb :reversename
+  end
+
+  get '/square/:number' do
+    erb :square
   end
 
 end
